2b455bbdd5e1a360326fb1668cfb47eeab0172cc
bravenbright <bbryantcs@gmail.com>
2023-04-15 00:23:45 -0500

    Updated portfolio and added many different features. working works, resume and about page

---
+++ react-porfolio/src/components/Layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import { Container } from "@mui/material";
+import Footer from "./Footer.js";
+
+function Layout(props) {
+  return (
+    <>
+      <Header />
+      <Container fixed>{props.children}</Container>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
